6c428609c1bf81c805ae02647eaf7f2052c870b5
luigialmeyda <luisfra@gmail.com>
2026-09-16 15:32:04 +0100

    UDP: add provisional timeout logic

--- udp.c
+++ udp.c
@@ -1,14 +1,28 @@
 
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/time.h>
 
+#include "udp.h"
 #include "app_state.h"
 
 int udp_setup(AppState *state, char *ds_ip, char *ds_port) {
 
+    int errcode;
+    struct addrinfo hints;
+    struct timeval tv;
+
     state->udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (state->udp_fd == -1) {
         perror("Erro ao criar o socket");
+        return -1;
+    }
+
+    tv.tv_sec = UDP_TIMEOUT;
+    tv.tv_usec = 0;
+    if (setsockopt(state->udp_fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) == -1) {
+        perror("Erro ao definir o timeout do socket UDP");
+        close(state->udp_fd);
         return -1;
     }
 
@@ -36,7 +50,7 @@ ssize_t send_and_recv(AppState *state, char *request, char *reply_buffer) {
     n = send_to(state->udp_fd, request, strlen(request), 0, 
                 state->ds_addr->ai_addr, state->ds_addr->ai_addrlen);
     if (n == -1) {
-        perror("Erro ao enviar pedido.");
+        perror("Erro ao enviar pedido");
         return -1;
     }
 
@@ -45,8 +59,8 @@ ssize_t send_and_recv(AppState *state, char *request, char *reply_buffer) {
     n = recvfrom(state->udp_fd, reply_buffer, sizeof(reply_buffer), 0, 
                 (struct sockaddr *) &from, &addrlen);
     if (n == -1) {
-        // TODO: Como lidar com o timeout?
-        perror("Erro ao receber pedido.");
+        // TODO: Não estamos a lidar com o timeout! Como o fazer?
+        perror("Erro ao receber pedido");
         return -1;
     }
 
--- udp.h
+++ udp.h
@@ -6,7 +6,6 @@
 #define UDP_TIMEOUT 5
 
 int udp_setup(AppState *state, const char *ds_ip, const char *ds_port);
-
 int send_and_recv(AppState *state, char *request, char *reply_buffer);
 
 #endif
