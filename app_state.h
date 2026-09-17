2b9c95954b8a97b33819b3afb5d03fe376829b55
Luís Almeida <luisfra@gmail.com>
2026-09-18 00:41:49 +0100

    AppState: include-related fix

--- app_state.h
+++ app_state.h
@@ -3,18 +3,19 @@
 
 #include <netdb.h>
 
+#include "user.h"
+
 typedef struct {
     // User state (login status, UID and password).
     User user;
 
-    int peer_tcp_port; // TODO: What about this one? app start up vs login params?
+    int peer_tcp_port;
 
     // DS address (set at app startup).
     struct addrinfo *ds_addr;
 
     // UDP file descriptor.
     int udp_fd;
-
 } AppState;
 
 #endif
