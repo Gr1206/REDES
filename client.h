4c51e09c70557d8a1591b12662ab129056b118d6
Gonçalo <goncalo06.rafael@gmail.com>
2026-09-19 11:17:17 +0100

    Client_udp.c renamed && Added header file for client_udp.c (client.h) && Documentation

---
+++ client.h
@@ -0,0 +1,44 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <netdb.h>
+
+/**
+ * @brief Signal handler for SIGINT
+ */
+void handle_sigint(int sig);
+
+/**
+ * @brief Controlled exit of the application
+ * @param socket_fd The socket file descriptor to close (if opened)
+ * @param exit_code The exit code to return
+ * @param res The addrinfo structure to free (if allocated)
+ */
+void controlledExit(int socket_fd, int exit_code, struct addrinfo *res);
+
+
+/**
+ * @brief Check if the UID has 6 digits
+ * @param uid The UID to check
+ * @return 1 if the UID is valid, 0 invalid 
+ */
+int checkUID(char* uid);
+
+/**
+ * @brief Check if the password has 8 alphanumeric chars
+ * @param password The password to check
+ * @return 1 if the password is valid, 0 invalid
+ */
+int checkPassword(char* password);
+
+
+/**
+ * @brief Check if the port is in the range 1-65535
+ * @param port The port to check
+ * @return 1 if the port is valid, 0 invalid
+ */
+int checkPort(int port);
+
+
+
+#endif // CLIENT_H
--- client_udp.c
+++ client_udp.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
 #include <unistd.h>
 #include <ctype.h>
 #include <sys/types.h>
@@ -13,6 +12,7 @@
 #include "user.h"
 #include "commands.h"
 #include "app_state.h"
+#include "client.h"
 
 #define MAX_ARGS 10
 
@@ -20,16 +20,16 @@ volatile sig_atomic_t stop_req = 0;
 
 void handle_sigint(int sig) {
     (void)sig;
-    stop_req = 1;
+    stop_req = 1; //SIGINT received
 }
+
 void controlledExit(int socket_fd, int exit_code, struct addrinfo *res) {
     if(res != NULL) freeaddrinfo(res);
-    if(socket_fd >= 0) close(socket_fd); //verificar caso criação do socket falhe
+    if(socket_fd >= 0) close(socket_fd); //close the socket if it was opened
     exit(exit_code);
 }
 
 int checkUID(char* uid){
-    //verificar se o UID é válido
     int strsize = strlen(uid);
     if(strsize != 6) return 0; 
 
@@ -41,7 +41,6 @@ int checkUID(char* uid){
 }
 
 int checkPassword(char* password){
-    //verificar se a password é válida
     int strsize = strlen(password);
     if(strsize != 8) return 0;
 
@@ -53,7 +52,6 @@ int checkPassword(char* password){
 }
 
 int checkPort(int port){
-    //verificar se a porta é válida
     if(port < 1 || port > 65535) return 0;
     return 1;
 }
@@ -61,28 +59,29 @@ int checkPort(int port){
 int main(int argc, char *argv[]){
     struct User user = {"", "", 0}; 
     AppState state = {user, 0, NULL, -1}; //default
-    char* DSIP = "193.136.138.142"; //default 
-    char* DSPORT = "59000"; //default
+    char* DSIP = "193.136.138.142";       //default 
+    char* DSPORT = "59000";               //default
     struct addrinfo hints;
 
     char *args[MAX_ARGS];
     int argcount = 0;
 
-    signal(SIGPIPE, SIG_IGN);
+    signal(SIGPIPE, SIG_IGN);            //ignore SIGPIPE
     struct sigaction sa;
-    memset(&sa, 0, sizeof(sa)); //incializa a zero
+    memset(&sa, 0, sizeof(sa));          //initialize to zero
     sa.sa_handler = handle_sigint; 
-    sigaction(SIGINT, &sa, NULL); 
+    sigaction(SIGINT, &sa, NULL);        //handle SIGINT
     
-    //caso passe flag sem nada
+    // Check if user provided a correct number of arguments
     if (argc % 2 == 0) {
-        printf("Formato correto: ./user -m peerport [-n DSIP] [-p DSport]\n");
+        printf("Incorrect format: ./user -m peerport [-n DSIP] [-p DSport]\n");
         exit(1);
     }
+
     // ./user -m peerport [-n DSIP] [-p DSport]
     for(int i = 1; i < argc; i += 2){
-        if (argv[i][0] != '-') {
-            printf("Formato correto : ./user -m peerport [-n DSIP] [-p DSport]\n");
+        if (argv[i][0] != '-') { //look for flag
+            printf("Incorrect format: ./user -m peerport [-n DSIP] [-p DSport]\n");
             exit(1);
         }   
         switch (argv[i][1]){
@@ -96,24 +95,25 @@ int main(int argc, char *argv[]){
                 DSPORT = argv[i+1];
                 break;
             default:
-                printf("Comando inválido\n./user -m peerport [-n DSIP] [-p DSport]\n");
+                printf("Flag not recognized\n./user -m peerport [-n DSIP] [-p DSport]\n");
                 exit(1);
         }
     }
 
     if (state.peer_tcp_port == 0) {
-        printf("Porta do peer é obrigatória\n");
+        printf("Peer port is required\n");
         exit(1);
     }
 
     if (!checkPort(state.peer_tcp_port)) {
-        printf("Porta do peer inválida\n");
+        printf("Invalid peer port\n");
         exit(1);
     }
 
-    state.udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    state.udp_fd = socket(AF_INET, SOCK_DGRAM, 0);  //udp socket
     if (state.udp_fd == -1) {
-        perror("Erro ao criar socket");
+        perror("Error creating UDP socket");
         controlledExit(state.udp_fd, 1, state.ds_addr);
     }
 
@@ -123,17 +123,15 @@ int main(int argc, char *argv[]){
 
 
     int errcode = getaddrinfo(DSIP, DSPORT, &hints, &state.ds_addr);
-    if (errcode != 0) { //meter mensagem de erro
-        fprintf(stderr, "Erro ao obter endereço\n");
+    if (errcode != 0) { 
+        fprintf(stderr, "Error getting address\n"); 
         controlledExit(state.udp_fd, 1, state.ds_addr);
     }
 
     while(1){
         char line[256];
-        // EDIT: Adicionei a validação para o caso do stdin ficar indisponível 
-        // entretanto e line não ser inicializada!
-        if (fgets(line, sizeof(line), stdin) == NULL) {
-            if(stop_req) {
+        if (fgets(line, sizeof(line), stdin) == NULL) { //unable to read input
+            if(stop_req) {                              //handle SIGINT
                 if(state.user.loggedIn) {
                     logout(state.udp_fd, state.ds_addr, &state.user);
                 }
@@ -143,7 +141,7 @@ int main(int argc, char *argv[]){
             break;
         }
 
-        //separar por espaços 
+        //Parse the input into args
         argcount = 0;
         char *token = strtok(line, " \n");
         while (token != NULL && argcount < MAX_ARGS) {
@@ -151,32 +149,28 @@ int main(int argc, char *argv[]){
             token = strtok(NULL, " \n");
         }
 
-        //tratr os args recebidos
+        
 
-        if(argcount == 0) { //nada escrito
+        if(argcount == 0) { //nothing written
             continue; 
         }
 
         char *command = args[0];
-        //para cada comando verificar se recebeu o número certo de argumentos
         if(strcmp(command, "login") == 0) {
-            if(argcount != 3) {     // EDIT: tem de ser apenas 3 login UID password, isto tem a ver com o parsing do comando, a peerport já está guardada
-                printf("Número de argumentos inválido: login UID password\n");
+            if(argcount != 3) {    
+                printf("Invalid number of arguments for login: login UID password\n");
                 continue;
             }
-            //chamar função de login
-            // EDIT: Acrescentei aqui uma lógica meio martelada para a peerport (só para funcionar!)
-
-            //EDIT : tirei a lógica, a peerport já está guardada, não está no arg do login
+            
             if(checkUID(args[1]) && checkPassword(args[2])){
                 login(state.udp_fd, state.ds_addr, &state.user, args[1], args[2], state.peer_tcp_port);
             } else {
-                printf("Formato de UID ou Password inválido\n");
+                printf("UID or password format is invalid\n");
             }
 
         } else if(strcmp(command, "unregister") == 0) {
             if(argcount != 1){
-                printf("Número de argumentos inválido: unregister\n");
+                printf("Invalid number of arguments for unregister: unregister\n");
                 continue;
             }
             unregister(state.udp_fd, state.ds_addr, &state.user);
@@ -184,25 +178,24 @@ int main(int argc, char *argv[]){
         } else if(strcmp(command, "logout") == 0) {
             // logout
             if(argcount != 1){
-                printf("Número de argumentos inválido: logout\n");
+                printf("Invalid number of arguments for logout: logout\n");
                 continue;
             }
             logout(state.udp_fd, state.ds_addr, &state.user);
 
         } else if(strcmp(command, "exit") == 0) {
             if(argcount != 1){
-                printf("Número de argumentos inválido: exit\n");
+                printf("Invalid number of arguments for exit: exit\n");
                 continue;
             }
             if(state.user.loggedIn) {
-                printf("É necessário dar logout antes de sair\n");
+                printf("It is required to logout before exiting\n");
                 continue;
             } else {
                 controlledExit(state.udp_fd, 0, state.ds_addr);
-                //break; 
             }
         } else {
-            printf("Comando não reconhecido\nLista de comandos válidos:\n-login\n-logout\n-unregister\n-exit\n");
+            printf("Command not recognized\nList of valid commands:\n-login\n-logout\n-unregister\n-exit\n");
         }
 
     }   
